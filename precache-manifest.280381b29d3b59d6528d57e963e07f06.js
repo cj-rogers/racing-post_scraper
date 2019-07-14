90c9bded736943dc0f65e450543fe892c17fcf46
Chris Rogers <chris.rogers@chriss-mbp.home>
2019-07-15 00:08:49 +0100

    Updates

---
+++ precache-manifest.280381b29d3b59d6528d57e963e07f06.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6a8654b4c64a86731571f7b65c6ab4",
+    "url": "/racing-post_scraper/index.html"
+  },
+  {
+    "revision": "8f379faa90f0ec4a0184",
+    "url": "/racing-post_scraper/static/js/2.4dd9fb28.chunk.js"
+  },
+  {
+    "revision": "8b79a19c33311b10cad9",
+    "url": "/racing-post_scraper/static/js/main.b71e6719.chunk.js"
+  },
+  {
+    "revision": "2588d131983d0ba60e6d",
+    "url": "/racing-post_scraper/static/js/runtime~main.08802ac3.js"
+  }
+]);
